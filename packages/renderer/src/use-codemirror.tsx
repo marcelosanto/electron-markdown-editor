03b87c48a8d11d35c6c3ee684400fc81db8653b3
Marcelo Santos <rocha.mer21@gmail.com>
2021-11-10 16:22:19 -0300

    config

--- packages/renderer/src/use-codemirror.tsx
+++ packages/renderer/src/use-codemirror.tsx
@@ -15,6 +15,31 @@ import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
 import { languages } from '@codemirror/language-data'
 import { oneDark } from '@codemirror/theme-one-dark'
 
+export const transparentTheme = EditorView.theme({
+  '&': {
+    backgroundColor: 'transparent !important',
+    height: '100%'
+  }
+})
+
+const syntaxHighlighting = HighlightStyle.define([
+  {
+    tag: tags.heading1,
+    fontSize: '1.6em',
+    fontWeight: 'bold'
+  },
+  {
+    tag: tags.heading2,
+    fontSize: '1.4em',
+    fontWeight: 'bold'
+  },
+  {
+    tag: tags.heading3,
+    fontSize: '1.2em',
+    fontWeight: 'bold'
+  }
+])
+
 import type React from 'react'
 
 interface Props {
@@ -43,6 +68,14 @@ const useCodeMirror = <T extends Element>(
         bracketMatching(),
         defaultHighlightStyle.fallback,
         highlightActiveLine(),
+        markdown({
+          base: markdownLanguage,
+          codeLanguages: languages,
+          addKeymap: true
+        }),
+        oneDark,
+        transparentTheme,
+        syntaxHighlighting,
         EditorView.lineWrapping,
         EditorView.updateListener.of(update => {
           if (update.changes) {
@@ -56,7 +89,6 @@ const useCodeMirror = <T extends Element>(
       state: startState,
       parent: refContainer.current
     })
-
     setEditorView(view)
   }, [refContainer])
 
